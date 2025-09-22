7a15af6201542ebb152d89fca16da5786b13af4f
onvei <sadeghi@nooran.com>
2025-09-22 00:23:01 -0700

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>IT Sales CRM MVP</h1>
+      <p>Front-end placeholder for CRM.</p>
+    </main>
+  );
+}
